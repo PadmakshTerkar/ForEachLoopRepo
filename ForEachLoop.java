4ef0f7d76c4e330ffe460014a493d7a381d0d4d5
Padmaksh Terkar <padmakshterkar96@gmail.com>
2024-04-20 23:32:33 +0530

    ForEach Loop Example

---
+++ ForEachLoop.java
@@ -0,0 +1,12 @@
+public class ForEachLoop {
+    public static void main(String args[])
+    {
+        int numbers[]={10,20,30,40,50};
+
+        //iterating through the array
+        for(int number : numbers)
+        {
+            System.out.print(" "+ number);
+        }
+    }
+}
